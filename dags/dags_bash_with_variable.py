0cd6866c9eaf5d178e6184ff9e9e5a53aefe5165
NB-4002245\DKSYSTEMS <vksk0258@gmail.com>
2024-07-19 14:07:16 +0900

    배시 전역변수 테스트 덱 생성

---
+++ dags/dags_bash_with_variable.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.models import Variable
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_with_variable",
+    # 이덱은 매일 6시 30분에 시작
+    schedule="10 9 * * *",
+    # 덱이 언제 부터 돌지 TZ=타임존
+    start_date=pendulum.datetime(2024, 7, 1, tz="Asia/Seoul"),
+    # 캐치업 배치 중간에 누락된 구간을 돌릴지 말지
+    # 1월 1일부터 3월 1일까지 누락된 덱을 한번에 돌아가게 된다 3월 1일에 웬만하면 false
+    catchup=False
+) as dag:
+    var_value = Variable.get("sample_key")
+
+    bash_var_1 = BashOperator(
+        task_id="bash_var_1",
+        bash_command=f"echo variable: {var_value}",
+    )
+
+    bash_var_2 = BashOperator(
+        task_id="bash_var_2",
+        bash_command=f"echo variable: {{ var.value.sample_key }}",
+    )
+
+    bash_var_1 >> bash_var_2
