48873af0f27e8203e57d2335a3648da2e845a381
NB-4002245\DKSYSTEMS <vksk0258@gmail.com>
2024-07-19 17:19:30 +0900

    팁 추가

--- dags/dags_python_with_task_group.py
+++ dags/dags_python_with_task_group.py
@@ -21,6 +21,7 @@ with DAG(
         msg = kwargs.get('msg') or ''
         print(msg)
 
+    # task_id가 다르더라도 그룹이 가능하면 사용할 수 있다!
     @task_group(group_id='firs_group')
     def group_1():
         '''task_group 데커레이터를 이용한 첫 번째 그룹입니다.'''
