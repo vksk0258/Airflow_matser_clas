9eebb246cbbb1f12ca8dca662413116272bb7265
NB-4002245\DKSYSTEMS <vksk0258@gmail.com>
2024-07-19 16:26:39 +0900

    수정

--- dags/dags_python_with_trigger_rule_eg1.py
+++ dags/dags_python_with_trigger_rule_eg1.py
@@ -35,4 +35,4 @@ with DAG(
     def python_downstream_1():
         print('정상처리')
 
-    [bash_upstream_1,python_upstream_1(), python_upstream_2()] >> python_upstream_1()
+    [bash_upstream_1,python_upstream_1(), python_upstream_2()] >> python_downstream_1()
