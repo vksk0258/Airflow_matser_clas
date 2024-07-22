bfb71dcd5b44b04dd0fa26b875f59f57e556b3c0
NB-4002245\DKSYSTEMS <vksk0258@gmail.com>
2024-07-22 10:03:58 +0900

    덱 수정

--- dags/dags_simple_http_operator.py
+++ dags/dags_simple_http_operator.py
@@ -8,14 +8,13 @@ with DAG(
         dag_id='dags_simple_http_operator',
         start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
         catchup=False,
-        schedule=None,
-        tags=["인프런",'python','api','공공데이터']
+        schedule=None
 ) as dag:
-    '''서울시 위생업소 전체 행정처분내역 현황'''
-    SeoulAdminMesure_info = SimpleHttpOperator(
+    '''서울시 공공자전거 대여소 정보'''
+    tb_cycle_station_info = SimpleHttpOperator(
         task_id='tb_cycle_station_info',
         http_conn_id='openapi.seoul.go.kr',
-        endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/json/SeoulAdminMesure/1/5',
+        endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/json/tbCycleStationInfo/1/10/',
         method='GET',
         headers={'Content-Type': 'application/json',
                  'charset': 'utf-8',
@@ -27,11 +26,11 @@ with DAG(
     @task(task_id='python_2')
     def python_2(**kwargs):
         ti = kwargs['ti']
-        rslt = ti.xcom_pull(task_ids='SeoulAdminMesure_info')
+        rslt = ti.xcom_pull(task_ids='tb_cycle_station_info')
         import json
         from pprint import pprint
 
-        pprint(json.dumps(rslt))
+        pprint(json.loads(rslt))
 
 
-    SeoulAdminMesure_info >> python_2()
+    tb_cycle_station_info >> python_2()
