52aeb0dc99dce714e3b045338ac667c77d371034
NB-4002245\DKSYSTEMS <vksk0258@gmail.com>
2024-07-25 13:11:24 +0900

    수정

--- dags/dags_snowflake_test.py
+++ dags/dags_snowflake_test.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from airflow.operators.python import PythonOperator
-import pandas as pd
+import pendulum
 
 def snowflake_conn_test():
     snowflake_hook = SnowflakeHook(snowflake_conn_id='snow_itsmart')
@@ -18,6 +18,7 @@ with DAG(
     # 이덱은 매일 6시 30분에 시작
     schedule="30 6 * * *",
     # 캐치업 배치 중간에 누락된 구간을 돌릴지 말지
+    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
     # 1월 1일부터 3월 1일까지 누락된 덱을 한번에 돌아가게 된다 3월 1일에 왠만하면 false
     catchup=False,
     tags=["옵션", "태그"]
