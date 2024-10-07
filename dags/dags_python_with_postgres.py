04026af3c398052a870f3e0b1da8950681c74305
NB-4002245\DKSYSTEMS <vksk0258@gmail.com>
2024-10-07 09:42:56 +0900

    dag 수정

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -26,7 +26,7 @@ with DAG(
     insrt_postgres = PythonOperator(
         task_id='insrt_postgres',
         python_callable=insrt_postgres,
-        op_args=['172.28.0.3', '5432', 'hjkim', 'hjkim', 'hjkim']
+        op_args=['172.28.0.3', '5432', 'giltaek', 'vksk0258', 'vlsl0258']
     )
 
     insrt_postgres
